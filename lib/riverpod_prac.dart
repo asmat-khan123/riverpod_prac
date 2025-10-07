7423cd90893c3187cf97af8023e90dd733c45d0a
Asmat Khan <asmatullahkhanmkl200@gmail.com>
2025-10-07 11:08:03 +0500

    project setup

---
+++ lib/riverpod_prac.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class riverpod_prac extends StatefulWidget {
+  const riverpod_prac({super.key});
+
+  @override
+  State<riverpod_prac> createState() => _riverpod_pracState();
+}
+
+class _riverpod_pracState extends State<riverpod_prac> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("Riverpod_prac"),
+    );
+  }
+}
